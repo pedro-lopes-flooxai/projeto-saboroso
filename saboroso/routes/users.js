2da8dc36f03fc4b694f0e992e63a26387105a373
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-07 10:10:46 -0300

    add: users

--- saboroso/routes/users.js
+++ saboroso/routes/users.js
@@ -1,9 +1,20 @@
+var conn = require('./../inc/db');
 var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+
+    conn.query("SELECT * FROM tb_users ORDER BY name", (err, result) => {
+
+      if (err) {
+        res.send(err);
+      } else {
+
+          res.send(results);
+      }
+
+    });
 });
 
 module.exports = router;
