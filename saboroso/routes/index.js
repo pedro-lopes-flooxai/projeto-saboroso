6f612989773f8fa6a8d4605899081b5dc6365a41
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-08 08:40:56 -0300

    add: includes for content and views

--- saboroso/routes/index.js
+++ saboroso/routes/index.js
@@ -1,25 +1,20 @@
 var conn = require('./../inc;db');
 var express = require('express');
+var menus = require('./../inc/menus');
 var router = express.Router();
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-  conn.query(`
-    SELECT * FROM tb_menus ORDER BY title
-    `,(err, results)=>{
+  menus.getMenus().then(results =>{
 
-      if (err) {
-        console.log(err);
-      }
-
-    });
-
-  res.render('index', { 
-    title: 'Restaurante Saboroso!',
-    menus: results 
+    res.render('index', { 
+      title: 'Restaurante Saboroso!',
+      menus: results 
+          });
+      });
   });
-});
 
 router.get('/contacts', function(req, res, next) {
 
@@ -33,12 +28,17 @@ router.get('/contacts', function(req, res, next) {
 
 router.get('/menus', function(req, res, next) {
 
-  res.render('menus', {
-    title: 'Menus - Restaurante Saboroso!',
-      background:'images/img_bg_1.jpg',
-        h1: 'Saboreie nosso menu!'
-});
+  menus.getMenus().then(results => {
 
+    
+    res.render('menus', {
+      title: 'Menus - Restaurante Saboroso!',
+        background:'images/img_bg_1.jpg',
+          h1: 'Saboreie nosso menu!',
+          menus: results
+
+        });
+    });
 });
 
 router.get('/reservations', function(req, res, next) {
