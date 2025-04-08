341e8b148b94f5c120b2d319fd199ec0db2b823e
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-08 10:57:40 -0300

    add: post for contacts

---
+++ saboroso/bin/contacts.js
@@ -0,0 +1,45 @@
+var conn = require("./db");
+
+moodule.exports = {
+
+    render(req, res, error, succes){
+
+        res.render('contacts', {
+            title: 'Contato - Restaurante Saboroso!',
+            background:'images/img_bg_3.jpg',
+            h1: 'Diga um oi!',
+            body: req.body,
+            error,
+            success
+        });
+
+    },
+
+    save(fields){
+
+            return new Promise((resolve, reject)=> {
+
+                conn.query(`
+                    INSERT INTO tb_contacts (name, email, message) 
+                    VALUES (?,?,?)
+                    `, [
+                            fields.name,
+                            fields.email,
+                            fields.message
+                    ], (err, rsults)=>{
+
+                        if (err) {
+
+                        reject (err);
+
+                        } else {
+
+                            resolve(results);
+
+                        }
+                    });
+
+            });
+    }
+
+}
