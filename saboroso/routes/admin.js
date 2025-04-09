f44b1dcf9da10f2853949e1f1d0c2a02f99f2082
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-08 23:07:20 -0300

    add: Redis

---
+++ saboroso/routes/admin.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var router = express.router();
+
+router.get("/", function(req, res, next){
+
+    res.render("admin/index");
+
+});
+
+router.get("/login", function(req, res, next){
+
+    if (!req.session.view) req.session.view = 0;
+
+    console.log("SESSION:", req.session.view++);
+
+    res.render("admin/login");
+
+});
+
+router.get("/contacts", function(req, res, next){
+
+    res.render("admin/contacts");
+
+});
+
+router.get("/emails", function(req, res, next){
+
+    res.render("admin/emails");
+
+});
+
+router.get("/menus", function(req, res, next){
+
+    res.render("admin/menus");
+
+});
+
+router.get("/reservations", function(req, res, next){
+
+    res.render("admin/reservations", {
+        date:{}
+    });
+
+});
+
+router.get("/users", function(req, res, next){
+
+    res.render("admin/users");
+
+});
+
+
+
+
+
+module.exports = router;
