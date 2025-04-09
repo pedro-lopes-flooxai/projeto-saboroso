3d571a36f5393f9d0e6e3fd35060610e66de4015
pedro-lopes-flooxai <pedro.lopes.flooxai@gmail.com>
2025-04-09 08:16:17 -0300

    add: links menu

--- saboroso/routes/admin.js
+++ saboroso/routes/admin.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var users = require("./../inc/users");
+var admin = require("./../inc/admin");
 var router = express.Router();
 
 router.use(function(req, res, next){
@@ -9,6 +10,14 @@ router.use(function(req, res, next){
     } else {
         next();
     }
+
+});
+
+router.use(function(req,res,next){
+
+    req.menus = admin.getMenus();
+
+    next();
 
 });
 
@@ -23,7 +32,9 @@ router.get("/logout", function(req, res, next){
 
 router.get("/", function(req, res, next){
    
-         res.render("admin/index");
+         res.render("admin/index", {
+            menus: req.menus
+         });
   
 });
 
@@ -60,33 +71,41 @@ router.get("/login", function(req, res, next){
 
 router.get("/contacts", function(req, res, next){
 
-    res.render("admin/contacts");
-
+    res.render("admin/contacts", {
+        menus: req.menus
+    });
 });
 
 router.get("/emails", function(req, res, next){
 
-    res.render("admin/emails");
+    res.render("admin/emails", {
+        menus: req.menus
+    });
 
 });
 
 router.get("/menus", function(req, res, next){
 
-    res.render("admin/menus");
+    res.render("admin/menus", {
+        menus: req.menus
+    });
 
 });
 
 router.get("/reservations", function(req, res, next){
 
     res.render("admin/reservations", {
-        date:{}
+        date:{},
+        menus: req.menus
     });
 
 });
 
 router.get("/users", function(req, res, next){
 
-    res.render("admin/users");
+    res.render("admin/users", {
+        menus: req.menus
+    });
 
 });
 
